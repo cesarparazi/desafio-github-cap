4f763306cb0aa805c3a2bd4c16983afe9e4f89d7
cesarparazi <ceparazi@gmail.com>
2026-01-28 06:49:41 -0300

    adicionando basico.git.sh

---
+++ basico_git.sh
@@ -0,0 +1,14 @@
+echo "1. Criar o arquivo;"
+echo "Exemplo: Conteúdo do arquivo > novo_arquivo.txt"
+
+echo "2. Verificar o status (opcional, mostra o arquivo como untracked)"
+echo "git status"
+
+echo "3. Adicionar o arquivo para o commit (stage)"
+echo "git add novo_arquivo.txt"
+
+echo "4. Confirmar (commit) o arquivo"
+echo "git commit -m Criando novo arquivo"
+
+echo "5. Enviar para o repositório remoto (se necessário)"
+echo "git push"
