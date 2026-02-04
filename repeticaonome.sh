41c078313ea7ce788733f06f21a4f9a26e117bb1
cesarparazi <ceparazi@gmail.com>
2026-02-04 19:15:47 -0300

    repete o nome também,

---
+++ repeticaonome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+var="Hello World!!"
+for cont in 1 2 3 4 5
+do
+echo $var
+done
+echo "Qual o seu nome?"
+read nome
+for cont in 1 2 3 4 5 6
+do
+echo "Hello" $nome"!! Welcome!!"
+done
+
